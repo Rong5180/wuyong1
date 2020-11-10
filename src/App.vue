20a318bf3191d345dd1a90b803ea23375a453dc2
Rong <2441238004@qq.com>
2020-11-10 22:34:59 +0800

    '111'

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <span>5645486</span>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+  export default {
+    name:'app',
+    components:{
+    
+    }
+  }
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
