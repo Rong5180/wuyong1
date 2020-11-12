c837ec95c673612016e1ad120b29a9c7f9e68606
Rong <2441238004@qq.com>
2020-11-12 09:22:57 +0800

    '222'

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-  
+   <router-view/>
+  <main-tab-bar/>
   </div>
 </template>
 
 <script>
+  import MainTabBar from "./components/content/mainTabbar/MainTabBar";
+
   export default {
     name:'app',
     components:{
-    
+    MainTabBar,
     }
   }
 </script>
 
 <style>
-#app {
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import './assets/css/base.css';
 </style>
